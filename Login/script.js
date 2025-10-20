e1ed329c90e39157849f580e73582e141dfd275c
MIGUELNEHEMIASPOLEOD <MIGUELNEHEMIASPOLEOD@SCS217763>
2025-10-20 20:05:47 -0300

    codigo incial do login

--- Login/script.js
+++ Login/script.js
@@ -1,8 +1,32 @@
+// const baseUrl = "http://localhost:5257"
+
+// const headers ={
+//             "content-type":	"application/json; charset=utf-8"
+//         }
+// async function getUsers(){
+//     const response = await fetch(`${baseUrl}/api/Usuario
+// `)
+//     const users = await response.json()
+
+// }
+// getUsers()
+
 function Login() {
-    const loginButton = document.querySelector('.login-button');
-    
-    loginButton.addEventListener('click', () => {
-        window.location.href = '../Home/index.html';
+    const loginButton = document.querySelector('form');
+    const nome = document.querySelector("email");
+        const senha = document.querySelector("password");
+        const modal = document.querySelector(".Modal");
+    loginButton.addEventListener('submit',async (e) => {
+        
+        e.preventDefault()
+        if(nome =='Miguel' && senha == '1234'){
+            window.location.href = '../Home/index.html';
+
+        }else{
+             modal.style.display = "block";
+             
+        }
+        console.log('Login button clicked');
     });
    
 }
