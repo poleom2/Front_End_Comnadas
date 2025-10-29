dd2b5fd55128b066212c639f2f067049c80d97ee
MIGUELNEHEMIASPOLEOD <MIGUELNEHEMIASPOLEOD@SCS217763>
2025-10-28 21:56:14 -0300

    Modificção da tela de login e comenzo da tela cardapio

--- Cardapio/script.js
+++ Cardapio/script.js
@@ -1,2 +1,9 @@
-import {openModalDesc} from "./modal.js"
+import { openModalDesc } from "./modal.js"
 openModalDesc()
+function listarCardapio() {
+    const listardocardapio = document.querySelector(".cardapio-lista")
+    fetch("http://localhost:3000/cardapio")
+        .then(resposta => resposta.json())
+
+}
+listarCardapio()
