d31c720823de619e26294e211f292403ab60e56f
MIGUELNEHEMIASPOLEOD <MIGUELNEHEMIASPOLEOD@SCS217763>
2025-10-31 19:47:39 -0300

    codigo escript

--- Cardapio/script.js
+++ Cardapio/script.js
@@ -1,9 +1,25 @@
 import { openModalDesc } from "./modal.js"
+const baseUrl = "http://localhost:5257/"
+const heders = {
+    "Content-Type": "application/json "
+}
 openModalDesc()
-function listarCardapio() {
+async function listarCardapio() {
     const listardocardapio = document.querySelector(".cardapio-lista")
-    fetch("http://localhost:3000/cardapio")
-        .then(resposta => resposta.json())
-
+    const ul = document.createElement("ul")
+    listarCardapio.appendChild(ul)
+    const respsta = await fetch("http://localhost:5257/swagger")
+    const cardapio = await respsta.json()
+    cardapio.forEach(element => {
+        const li = document.createElement("li")
+        li.classList.add("cardapio-item")
+        li.innerHTML = `
+        <h3 class="cardapio-nome">${element.nome}</h3>
+        <p class="cardapio-descricao">${element.descricao}</p>
+        <span class="cardapio-preco">R$ ${element.preco}</span>
+        <button class="cardapio-vermais" id="${element.id}">Ver mais</button>
+        `
+        ul.appendChild(li)
+    })
 }
 listarCardapio()
