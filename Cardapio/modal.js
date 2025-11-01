16582677f4d5c45a2783baf30c965f2938cd7fa7
MIGUELNEHEMIASPOLEOD <MIGUELNEHEMIASPOLEOD@SCS217763>
2025-10-31 21:24:57 -0300

    Criação do java escript do cardapipo criação do modal descrição e editar Funcionado

--- Cardapio/modal.js
+++ Cardapio/modal.js
@@ -1,7 +1,57 @@
-export function openModalDesc(){
+import { listarCardapio } from "./script.js";
+
+export function openModalDesc(element) {
     //abri o modal 1
     console.log("Modal de descrição aberto");
+    const modal = document.querySelector('.modal');
+    modal.style.display = 'block';
+    modal.innerHTML = `
+    <div class="modal-conteudo">
+        <span class="modal_fechar">&times;</span>
+
+        <p class="cardapio-descricao">${element.descricao}</p>
+        <img src="${element.ImageUrl}" alt="">
+        <button class="modal_editar">Editar</button>
+    </div>
+    `;
+    const modal_editar = modal.querySelector('.modal_editar');
+    modal_editar.addEventListener('click', (event) => {
+
+        openModalEdit(element)
+    });
+    const fecharBtn = modal.querySelector('.modal_fechar');
+
+    fecharBtn.addEventListener('click', (event) => {
+        modal.style.display = 'none';
+
+
+    });
+
+
+
 }
-export function openModalEdit(){
-    
+export function openModalEdit(element) {
+    const modal = document.querySelector('.modal');
+    modal.innerHTML = `
+    <input type="text" id="edit_description" value="${element.descricao}">
+    <input type="text" id="edit_ImageUrl" value="${element.ImageUrl}">
+    <input type="number" id="edit_price" value="${element.preco}">
+    <input type="text" id="edit_nome" value="${element.nome}">
+    <input type="text" id="edit_tipo" value="${element.tipo}">
+    <button class="modal_salvar">Salvar</button>
+    `;
+    const modal_salvar = modal.querySelector('.modal_salvar');
+    modal_salvar.addEventListener('click', (event) => {
+        element.description = document.querySelector('#edit_description').value;
+        element.ImageUrl = document.querySelector('#edit_ImageUrl').value;
+        element.price = parseFloat(document.querySelector('#edit_price').value);
+        element.nome = document.querySelector('#edit_nome').value;
+        element.tipo = document.querySelector('#edit_tipo').value;
+        console.log(element);
+        //put
+        modal.style.display = 'none';
+        listarCardapio()
+    });
+
+
 }
--- Cardapio/script.js
+++ Cardapio/script.js
@@ -3,23 +3,52 @@ const baseUrl = "http://localhost:5257/"
 const heders = {
     "Content-Type": "application/json "
 }
-openModalDesc()
-async function listarCardapio() {
-    const listardocardapio = document.querySelector(".cardapio-lista")
+const list = [
+    { id: "1", nome: "X-Burguer", descricao: "Pão, hambúrguer, queijo, alface, tomate e maionese", preco: 15.00, ImageUrl: "https://t4.ftcdn.net/jpg/06/46/48/07/240_F_646480739_5zBTxkCQnOpIEnjBJtFs5I7pkx4KcC0m.jpg  ", tipo: "Lanche" }
+    , { id: "2", nome: "X-Salada", descricao: "Pão, hambúrguer, queijo, alface, tomate, maionese e salada", preco: 18.00, ImageUrl: "https://br.freepik.com/fotos-premium/close-up-de-um-hamburguer-na-mesa_99999176.htm#from_element=cross_selling__photo,", tipo: "Lanche" }
+    , { id: "3", nome: "X-Bacon", descricao: "Pão, hambúrguer, queijo, alface, tomate, maionese e bacon", preco: 20.00, ImageUrl: "https://br.freepik.com/fotos-gratis/closeup-tiro-de-um-hamburguer-com-bacon-e-queijo-uma-caneca-de-cafe-vermelha_8280993.htm#fromView=keyword&page=1&position=1&uuid=461efc52-9893-4821-aa74-c663f6a5ad5c&query=Hamburger" }
+    , { id: "4", nome: "X-Egg", descricao: "Pão, hambúrguer, queijo, alface, tomate, maionese e ovo", preco: 22.00 }
+    , { id: "5", nome: "X-Tudo", descricao: "Pão, hambúrguer, queijo, alface, tomate, maionese, bacon e ovo", preco: 25.00 }
+
+]
+// openModalDesc()
+export async function listarCardapio() {
+    const listardocardapio = document.querySelector(".lista_cardapio")
+    listardocardapio.innerHTML = ""
     const ul = document.createElement("ul")
-    listarCardapio.appendChild(ul)
-    const respsta = await fetch("http://localhost:5257/swagger")
-    const cardapio = await respsta.json()
+    listardocardapio.append(ul)
+    // const respsta = await fetch("http://localhost:5257/api/CardapioItem")
+    const cardapio = list
     cardapio.forEach(element => {
         const li = document.createElement("li")
         li.classList.add("cardapio-item")
         li.innerHTML = `
         <h3 class="cardapio-nome">${element.nome}</h3>
-        <p class="cardapio-descricao">${element.descricao}</p>
+       
         <span class="cardapio-preco">R$ ${element.preco}</span>
         <button class="cardapio-vermais" id="${element.id}">Ver mais</button>
         `
+        const vermaisBtn = li.querySelector('.cardapio-vermais');
+        vermaisBtn.addEventListener('click', () => {
+            openModalDesc(element)
+            // const modal = document.querySelector('.modal');
+            // modal.innerHTML = `
+            // <div class="modal-conteudo">
+            //     <span class="modal-fechar">&times;</span>
+            //     <p class="cardapio-descricao">${element.description}</p>
+
+            // </div>
+            // `;
+            // modal.style.display = 'block';
+            // const fecharBtn = modal.querySelector('.modal-fechar');
+            // fecharBtn.addEventListener('click', () => {
+            //     modal.style.display = 'none';
+            // }
+            // );
+        }
+        );
         ul.appendChild(li)
     })
 }
 listarCardapio()
+// openModalDesc()
