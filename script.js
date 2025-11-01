6357864d8ec34b25bb909ae86092365e2ec07237
GABRIELAUGUSTOKAPPENBERG <kappenberg4@gmail.com>
2025-10-31 21:57:33 -0300

    ajuste do layout do login

--- script.js
+++ script.js
@@ -13,42 +13,39 @@
 
 function Login() {
     const loginButton = document.querySelector('form');
-       
-    loginButton.addEventListener('submit',async (e) => {  
+
+    loginButton.addEventListener('submit', async (e) => {
         e.preventDefault()
         loginAlert()
     });
-   
+
 }
 Login();
-function toastify(tipo,message){
-     document.body.insertAdjacentHTML("beforeend", `
+function toastify(tipo, message) {
+    document.body.insertAdjacentHTML("beforeend", `
      <div class="toastify ${tipo}">
-            <p>${message}</p
-    >
+            <p>${message}</p>
      </div>
      `);
-     const toastify = document.querySelector('.toastify');
-     setTimeout(() => {
+    const toastify = document.querySelector('.toastify');
+    setTimeout(() => {
         toastify.remove();
-     },3000);
+    }, 3000);
 }
-function loginAlert(){
-   const email = document.querySelector("#email");
+function loginAlert() {
+    const email = document.querySelector("#email");
     const password = document.querySelector("#password");
     console.log(email.value);
-    console.log(password.value);    
-    if(email.value == 'Miguel' && password.value == '1234' || email.value == 'Gabriel' && password.value == '1234')
-        {
-       
-        toastify("sucesso","Login realizado com sucesso!");
+    console.log(password.value);
+    if (email.value == 'Miguel' && password.value == '1234' || email.value == 'Gabriel' && password.value == '1234') {
+
+        toastify("sucesso", "Login realizado com sucesso!");
         setTimeout(() => {
-        location.href = '../Home/index.html';
+            location.href = '../Home/index.html';
         }, 3000);
-     }
-    else
-        {
-            toastify("erro","Email ou senha incorretos!")
-       
-     }
+    }
+    else {
+        toastify("erro", "Email ou senha incorretos!")
+
+    }
 }
